a3b6347ae841e22998961fe671e3f3b8425577a2
姜峰 <jiang.f@Ctrip.com>
2017-11-29 10:13:23 +0800

    'save'

--- ts/src/components/ApiDataList.tsx
+++ ts/src/components/ApiDataList.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react'
-
+import { connect } from 'react-redux'
+import { FETCH_APIDATA } from '../store/request'
+import { GetQueryString } from '../util'
 interface ITProps {}
-interface ITState {}
+interface ITState {
+
+  // 接口id
+  apiId: string
+}
 
 export default class ApiDataList extends React.Component<ITProps, ITState> {
+  constructor(props: ITProps){
+    super(props)
+    this.state = {
+      apiId: null
+    }
+  }
+  componentDidMount() {
+    let apiId = GetQueryString('id')
+    
+    // 根据接口id去获取代理数据
+
+    this.setState({
+      apiId
+    })
+  }
   public render(): JSX.Element{
     return (
-      <div>ApiDataList</div>
+      <div style={{padding: 20}}>ApiDataList</div>
     )
   }
 }
