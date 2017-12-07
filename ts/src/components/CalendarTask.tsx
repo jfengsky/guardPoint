2f461619acc9259181e12fbd5578b3e6792cdff8
姜峰 <jiang.f@Ctrip.com>
2017-12-07 13:24:07 +0800

    'save'

--- ts/src/components/CalendarTask.tsx
+++ ts/src/components/CalendarTask.tsx
@@ -1,19 +1,79 @@
 import * as React from 'react'
-import { Calendar, Alert } from 'antd'
+import { connect } from 'react-redux'
+import { Calendar, Alert, Badge } from 'antd'
+// import * as moment from 'moment'
+import { ITInitialState, ITTodo } from '../interface'
 
+import { checkDateIsBetween } from '../util'
 
-interface ITProps { }
-interface ITState { }
+interface ITProps {
+  todoList: Array<ITTodo>
+}
+interface ITState {}
 
-export default class CalendarTask extends React.Component<ITProps, ITState> {
-  public render(): JSX.Element {
+class CalendarTask extends React.Component<ITProps, ITState> {
+  public render (): JSX.Element {
     return (
-      <div style={{ padding: 　10 }}>
-        <Alert message="Info Text" type="info" showIcon />
-        <Alert message="Warning" type="warning" showIcon />
-        <Calendar />
+      <div style={{ padding: 10 }}>
+        <Alert message='Info Text' type='info' showIcon />
+        <Alert message='Warning' type='warning' showIcon />
+        <Calendar dateCellRender={this.dateCellRender} />
       </div>
-
     )
   }
+
+  getListData = (value: any): Array<any> => {
+    let listData: Array<any> = []
+    let { todoList } = this.props
+    let currentDate = value.toArray()
+    let minSeconds = new Date(
+      currentDate[0],
+      currentDate[1],
+      currentDate[2]
+    ).getTime()
+    if (todoList.length) {
+      todoList.map(({ date, _id, title, done, tag }: ITTodo) => {
+        let isBetween = checkDateIsBetween(minSeconds, date)
+        if (isBetween) {
+          let type = done ? 'success' : 'warning'
+          listData.push({
+            type,
+            title,
+            _id
+          })
+        }
+      })
+    }
+    // switch(value.date()) {
+
+    // }
+
+    return listData
+  }
+
+  dateCellRender = (value: any) => {
+    const listData: Array<any> = this.getListData(value)
+    return (
+      <ul>
+        {listData.map(({ type, title, _id }: any) => {
+          return (
+            <li key={_id} data-id={_id} onClick={this.taskInfo}>
+              <Badge status={type} text={title} />
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  taskInfo = (e: any) => {
+    let taskId: any = e.currentTarget.getAttribute('data-id')
+    location.href = `/time?id=${taskId}`
+  }
 }
+
+const mapStateToProps = (state: ITInitialState) => ({
+  todoList: state.todoList
+})
+
+export default connect(mapStateToProps)(CalendarTask)
--- ts/src/util/index.ts
+++ ts/src/util/index.ts
@@ -1,9 +1,31 @@
 // 获取地址栏参数
 export const GetQueryString = (name: string) => {
-  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)")
+  let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
   let r = window.location.search.substr(1).match(reg)
   if (r != null) {
     return r[2]
   }
   return null
+}
+
+export const checkDateIsBetween = (
+  _currentDate: number,
+  dateList: Array<string>
+): boolean => {
+  let startDateArray: any = dateList[0].split('-')
+  let endDateArray: any = dateList[1].split('-')
+  let startSecond: number = new Date(
+    startDateArray[0] - 0,
+    startDateArray[1] - 1,
+    startDateArray[2]
+  ).getTime()
+  let endSecond: number = new Date(
+    endDateArray[0] - 0,
+    endDateArray[1] - 1,
+    endDateArray[2]
+  ).getTime()
+  if (_currentDate >= startSecond && _currentDate <= endSecond) {
+    return true
+  }
+  return false
 }
