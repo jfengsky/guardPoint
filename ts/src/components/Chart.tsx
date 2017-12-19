8cf3ec6c2dbace763075dbf9f6bdb9e152b0e791
姜峰 <jiang.f@Ctrip.com>
2017-12-19 14:03:56 +0800

    'save'

--- ts/src/components/Chart.tsx
+++ ts/src/components/Chart.tsx
@@ -8,7 +8,33 @@ interface ITState {}
 
 class Chart extends React.Component<ITProps, ITState> {
   componentDidMount () {
-    const main: any = document.getElementById('main')
+    this.taskfinished()
+    this.taskType()
+  }
+  public render (): JSX.Element {
+    const fullStyle = {
+      width: '100%',
+      height: '100%'
+    }
+    return (
+      <div>
+        <Row>
+          <Col span={12}>
+            <div id='taskFinished' style={fullStyle} />
+          </Col>
+          <Col span={12}>
+            <div id='taskType' style={fullStyle} />
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+
+  /**
+   * 已完成，未完成任务
+   */
+  taskfinished = () => {
+    const main: any = document.getElementById('taskFinished')
     let myChart = echarts.init(main)
     myChart.setOption({
       tooltip: {
@@ -24,7 +50,7 @@ class Chart extends React.Component<ITProps, ITState> {
         {
           name: '访问来源',
           type: 'pie',
-          radius: ['50%', '70%'],
+          radius: ['40%', '60%'],
           avoidLabelOverlap: false,
           label: {
             normal: {
@@ -49,21 +75,51 @@ class Chart extends React.Component<ITProps, ITState> {
       ]
     })
   }
-  public render (): JSX.Element {
-    const fullStyle = {
-      width: '100%',
-      height: '100%'
-    }
-    return (
-      <div>
-        <Row>
-          <Col span={12}>
-            <div id='main' style={fullStyle} />
-          </Col>
-          <Col span={12}>222</Col>
-        </Row>
-      </div>
-    )
+
+  /**
+   * 任务类型
+   */
+  taskType = () => {
+    const taskType: any = document.getElementById('taskType')
+    let myChart = echarts.init(taskType)
+    myChart.setOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        x: 'left',
+        data: ['项目', 'CR', 'bug', '事件', '紧急']
+      },
+      series: [
+        {
+          name: '任务类型',
+          type: 'pie',
+          radius: ['10%', '60%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data: [{ value: 335, name: '已完成' }, { value: 310, name: '未完成' }]
+        }
+      ]
+    })
   }
 }
 
